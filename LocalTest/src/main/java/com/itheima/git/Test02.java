f2fbe3248186be294bf58f9f734e26e82d5f877b
luoting <1982209038@qq.com>
2020-09-28 23:24:40 +0800

    添加了Test02类

---
+++ LocalTest/src/main/java/com/itheima/git/Test02.java
@@ -0,0 +1,16 @@
+package com.itheima.git;
+
+/**
+ * @author LuoTing
+ * date  2020 - 09- 28- 23:23
+ */
+public class Test02 {
+    public static void main(String[] args) {
+        System.out.println("HELLO 222222");
+        System.out.println("HELLO 222222");
+        System.out.println("HELLO 222222");
+        System.out.println("HELLO 222222");
+        System.out.println("HELLO 222222");
+        System.out.println("HELLO 222222");
+    }
+}
