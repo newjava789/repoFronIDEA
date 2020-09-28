08a9828061e6d5eea8a7a8a28b5ab95957632a6f
luoting <1982209038@qq.com>
2020-09-28 22:52:53 +0800

    添加了工程到本地仓库

---
+++ LocalTest/src/main/java/com/itheima/git/Test01.java
@@ -0,0 +1,16 @@
+package com.itheima.git;
+
+/**
+ * @author LuoTing
+ * date  2020 - 09- 28- 22:52
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+    }
+}
